7f59fe40b0b8217b321fc1c146b436d9ae5b735f
Christopher Davis <christopherjdavis@gmail.com>
2023-01-16 13:14:11 -0800

    Update odds_helper.py

--- odds_helper.py
+++ odds_helper.py
@@ -1,20 +1,6 @@
 import numpy as np
 import pandas as pd
 
-df_names = pd.read_csv("/Users/christopherdavis/Dropbox/Programming/NFLdata/NameReplacements.csv")
-df_names.set_index("Long", drop=True, inplace=True)
-series_names = df_names["Short"]
-
-df_div = pd.read_csv("/Users/christopherdavis/Dropbox/Programming/NFLdata/divisions.csv")
-df_div.columns = ["team", "div"]
-ser_div = df_div.set_index("team")["div"]
-
-def get_div(team):
-    return ser_div[team]
-
-def get_conf(team):
-    return ser_div[team][:3]
-
 def prob_to_odds(p):
     if p < .000001:
         return np.nan
@@ -34,11 +20,3 @@ def odds_to_prob(x):
         return y/(100+y)
     else:
         return 100/(100+x)
-    
-def get_abbr(long):
-    long = long.upper()
-    if long in series_names.index:
-        return series_names[long]
-    temp = long.split()[-1]
-    if temp in series_names.index:
-        return series_names[temp]
